9b49726a0708c11177985e5786e544e5d217f690
Mariia D <mariia.demidova0304@gmail.com>
2025-08-08 11:02:25 -0400

    Finished with TodoItem for now

--- src/assets/components/TodoItem/TodoItem.tsx
+++ src/assets/components/TodoItem/TodoItem.tsx
@@ -1,15 +1,33 @@
-import { Button, ButtonGroup, Container, InputGroup, ListGroupItem } from "react-bootstrap";
-import type { TodoItemProps, Todo } from "../../types";
+import { Button, ButtonGroup, Container, InputGroup } from "react-bootstrap";
+import type { TodoItemProps } from "../../types";
+import { useContext } from "react";
+import { TodoContext } from "../contexts/contexts";
 
+//element gets a task as a prop from parent - List, and displays it, 
+// while also allowing usage of context for editing, deleting and toggling
 export default function TodoItem({todo} : TodoItemProps) {
-   
+   const {editTodo, deleteTodo, toggleTodo} = useContext(TodoContext);
+
+    const handleToggle = () => {
+        toggleTodo(todo.id);
+    }
+
+   const HandleDelete = () => {
+    deleteTodo(todo.id);
+   }
+
+   //don't really know how to tackle the edit beast for now
+   const handleEdit = () => {
+    editTodo(todo.id, 'edited To Do');
+   }
+
     return(
         <Container>
-            <InputGroup.Radio />
-            <span>{todo.text}</span>
+            <InputGroup.Radio checked={todo.completed} onClick={handleToggle}/>
+            <span style={{textDecoration: todo.completed ? 'line-through' : 'none'}}>{todo.text}</span>
             <ButtonGroup>
-                <Button>✏️</Button>
-                <Button>🗑️</Button>
+                <Button onClick={handleEdit}>✏️</Button>
+                <Button onClick={HandleDelete}>🗑️</Button>
             </ButtonGroup>
         </Container>
     )
